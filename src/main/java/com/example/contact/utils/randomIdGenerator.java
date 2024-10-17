98f081b6d27de2a8099e2e97c7d3bf835db6a473
simon <2281823591@qq.com>
2024-10-18 01:07:23 +0800

    Changes

--- src/main/java/com/example/contact/utils/randomIdGenerator.java
+++ src/main/java/com/example/contact/utils/randomIdGenerator.java
@@ -1,4 +1,33 @@
 package com.example.contact.utils;
 
+import org.springframework.stereotype.Component;
+
+import java.security.SecureRandom;
+
+/**
+ * @author bianjiang
+ */
+@Component
 public class randomIdGenerator {
+    // 保证所有内存线程可见
+    private volatile static randomIdGenerator instance;
+    // 防止编译器自动生成一个public属性的构造方法
+    private randomIdGenerator(){}
+    public static randomIdGenerator getInstance(){
+        if(instance == null){
+            synchronized (randomIdGenerator.class){
+                if(instance == null){
+                    instance = new randomIdGenerator();
+                }
+            }
+        }
+        return instance;
+    }
+    public Long generateId(){
+        SecureRandom random = new SecureRandom();
+        long id=random.nextLong();
+        return id;
+    }
+
+
 }
--- src/test/java/com/example/contact/ContactApplicationTests.java
+++ src/test/java/com/example/contact/ContactApplicationTests.java
@@ -1,13 +1,17 @@
 package com.example.contact;
 
+import com.example.contact.utils.randomIdGenerator;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class ContactApplicationTests {
-
+    @Autowired
+    private randomIdGenerator idGenerator=randomIdGenerator.getInstance();
     @Test
-    void contextLoads() {
+    void test() {
+        System.out.println(idGenerator.generateId());
     }
 
 }
