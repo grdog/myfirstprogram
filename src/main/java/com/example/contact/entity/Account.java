0197057569e3e5aedde2fb03dd7837e3d830a93e
simon <2281823591@qq.com>
2024-10-18 00:05:09 +0800

    Changes

--- src/main/java/com/example/contact/entity/Account.java
+++ src/main/java/com/example/contact/entity/Account.java
@@ -1,5 +1,10 @@
 package com.example.contact.entity;
 
+import java.util.UUID;
+
 // 绑定均与account相关
 public class Account {
+    private UUID id;
+    private String loginName;
+    private String password;
 }
